2f1dd0f87075cbe70f817d3dd8f31f4d35b5d340
二磊 <dunaixiaoye@163.com>
2019-06-22 16:49:35 +0800

    本地工程添加到 Git

---
+++ dya01_git01/src/cn/itcast/tesst/Demo01_git.java
@@ -0,0 +1,17 @@
+package cn.itcast.tesst;
+
+public class Demo01_git {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
